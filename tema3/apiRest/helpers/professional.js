770f70426e00b5b6debcbfd0fda37d13d3fe72fb
CodenotchSchool <codenotch@codenotch.com>
2022-10-06 16:59:15 +0200

    Subida del Reto 2

---
+++ tema3/apiRest/helpers/professional.js
@@ -0,0 +1,13 @@
+class Professional
+{
+    constructor(fistName, lastName, proffesion, nationality, oscarsNumber)
+    {
+        this.fistName = fistName;
+        this.lastName = lastName;
+        this.proffesion = proffesion;
+        this.nationality = nationality;
+        this.oscarsNumber = oscarsNumber;
+    }
+}
+
+module.exports = {Professional};
