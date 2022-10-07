7f965e42565e4c59fd0d8459a37593935f6c90a6
CodenotchSchool <codenotch@codenotch.com>
2022-10-07 13:38:04 +0200

    Merge

---
+++ tema4/consumo_api_interna/professional.js
@@ -0,0 +1,11 @@
+class Professional
+{
+    constructor(fistName, lastName, proffesion, nationality, oscarsNumber)
+    {
+        this.nombre = fistName;
+        this.apellido = lastName;
+        this.profesion = proffesion;
+        this.nacionalidad = nationality;
+        this.numeroOscars = oscarsNumber;
+    }
+}
